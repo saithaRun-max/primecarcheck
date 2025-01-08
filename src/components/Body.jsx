ec2f38488eadd23fbdc425fbf018cbb170b449f4
Sai Tharun <128915167+saithaRun-max@users.noreply.github.com>
2025-01-08 19:09:35 +0530

    initial setup

---
+++ src/components/Body.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Home from "./Home";
+
+const Body = () => {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>
+  );
+};
+
+export default Body;
